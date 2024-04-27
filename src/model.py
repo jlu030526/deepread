9708cba82c1c129e9f4d44c95a2f6c025a6a2c19
Alex L <alexander_liang@brown.edu>
2024-04-26 20:53:21 -0400

    update

--- src/model.py
+++ src/model.py
@@ -1,3 +1,90 @@
 import tensorflow as tf
+
+import os
+import tensorflow as tf
 import numpy as np
+
+class Model(tf.keras.Model):
+    def __init__(self):
+        
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+        self.cnn_layer = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(),
+            tf.keras.layers.MaxPool2D(),
+            tf.keras.layers.Conv2D(),
+            tf.kears.layers.MaxPool2D()
+        ])
+
+        self.flatten = tf.keras.layers.Flatten()
+
+        self.rnn_layer = tf.keras.Sequential([
+            tf.keras.layers.LSTM(),
+            tf.keras.layers.Dropout(.2),
+            tf.keras.layers.LSTM(),
+            tf.keras.layers.Dropout(.2)
+        ])
+
+        self.ff_layer = tf.keras.layers.Sequential([
+            tf.keras.layers.Dense(units=0, activation='relu'),
+            tf.keras.layers.Dense(units=0, activation='relu'),
+        ])
+
+
+    def call(self, inputs, is_testing=False):
+        """
+        Runs a forward pass on an input batch of images.
+        
+        :param inputs: images, shape of (num_inputs, 32, 32, 3); during training, the shape is (batch_size, 32, 32, 3)
+        :param is_testing: a boolean that should be set to True only when you're doing Part 2 of the assignment and this function is being called during testing
+        :return: logits - a matrix of shape (num_inputs, num_classes); during training, it would be (batch_size, 2)
+        """
+        # Remember that
+        # shape of input = (num_inputs (or batch_size), in_height, in_width, in_channels)
+        # shape of filter = (filter_height, filter_width, in_channels, out_channels)
+        # shape of strides = (batch_stride, height_stride, width_stride, channels_stride)
+
+        l1_out = self.conv_step(inputs, self.kernel1, False, self.conv_args_layer1, **self.conv_fns_layer1)
+        l2_out = self.conv_step(l1_out, self.kernel2, False, self.conv_args_layer2, **self.conv_fns_layer2)
+        l3_out = self.conv_step(l2_out, self.kernel3, False, self.conv_args_layer3, **self.conv_fns_layer3)
+        l4_out = self.conv_step(l3_out, self.kernel4, is_testing, self.conv_args_layer4, **self.conv_fns_layer4)
+        # stu_output = output
+        # true_output = tf.nn.conv2d(x, kernel, **conv_args)
+        # np.testing.assert_allclose(stu_output, true_output, rtol=1e-5, atol=1e-8)
+        output = tf.reshape(l4_out, (l4_out.shape[0], -1)) # make it 2D for linear layers
+
+        l1_out = self.linear_step(output, self.weights1, self.bias1)
+        l2_out = self.linear_step(l1_out, self.weights2, self.bias2)
+        l3_out = self.linear_step(l2_out, self.weights3, self.bias3, use_dropout=False, use_relu=False)
+        prbs = tf.nn.softmax(l3_out)
+        
+        return prbs
+
+    def loss(self, logits, labels):
+        """
+        Calculates the model cross-entropy loss after one forward pass.
+        Softmax is applied in this function.
+        
+        :param logits: during training, a matrix of shape (batch_size, self.num_classes) 
+        containing the result of multiple convolution and feed forward layers
+        :param labels: during training, matrix of shape (batch_size, self.num_classes) containing the train labels
+        :return: the loss of the model as a Tensor
+        """
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits))
+        self.loss_list.append(loss)
+        return loss
+
+def train(model, train_inputs, train_labels):
+    #train model
+    pass
+
+def test(model, test_inputs, test_labels):
+    #test model
+    pass
+
+
+if __name__ == '__main__':
+    main()
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,51 +1,155 @@
 import tensorflow as tf
 import numpy as np
-from functools import reduce
+import os
+# import imutils
+# import dlib # run "pip install dlib"
+import cv2
+from PIL import Image
 
 #We could reference:
 #https://github.com/deepconvolution/LipNet/blob/master/codes/8_Preprocessing_model.ipynb
 
-#From HW4 
-def get_data(train_file, test_file):
+def preprocess_image(image_path):
     """
-    Read and parse the train and test file line by line, then tokenize the sentences to build the train and test data separately.
-    Create a vocabulary dictionary that maps all the unique tokens from your train and test data as keys to a unique integer value.
-    Then vectorize your train and test data based on your vocabulary dictionary.
-
-    :param train_file: Path to the training file.
-    :param test_file: Path to the test file.
-    :return: Tuple of:
-        train (1-d list or array with training words in vectorized/id form), 
-        test (1-d list or array with testing words in vectorized/id form), 
-        vocabulary (Dict containg index->word mapping)
-    """
-    # Hint: You might not use all of the initialized variables depending on how you implement preprocessing. 
-    vocabulary, vocab_size, train_data, test_data = {}, 0, [], []
-
-    ## TODO: Implement pre-processing for the data files. See notebook for help on this.
-
-    # Read and tokenize training data
-    with open(train_file, 'r') as training_file:
-        train_data = training_file.read().split()
-        unique_training_content = list(set(train_data))
-
-    # Read and tokenize testing data
-    with open(test_file, 'r') as testing_file:
-        test_data = testing_file.read().split()
-
-    # Create vocabulary mapping
-    vocabulary = {word: index for index, word in enumerate(unique_training_content)}
-
-    # Sanity Check, make sure there are no new words in the test data.
-    assert reduce(lambda x, y: x and (y in vocabulary), test_data)
     
-    # Uncomment the sanity check below if you end up using vocab_size
-    # Sanity check, make sure that all values are withi vocab size
-    # assert all(0 <= value < vocab_size for value in vocabulary.values())
+    """
+    
+    with Image.open(image_path) as img:
+        img_array = np.array(img.resize((300, 300)))
 
-    # Vectorize, and return output tuple.
-    train_data = [float(vocabulary[word]) for word in train_data]
-    test_data = [float(vocabulary[word]) for word in test_data]
+    src = cv2.imread(image_path) 
+    gray_image = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    img_array = np.array(gray_image.resize((300, 300)))
+    return gray_image
 
-    # print("train_data", train_data)
-    return train_data, test_data, vocabulary
+
+def preprocess():
+    """
+    loop per person ID e.g. F01
+        loop per phrase ID e.g. 01
+            array representing video frames
+            label for array
+            loop per instance of phrase (img) e.g. 01
+                video = []
+                for each image in video:
+                    crop img to lips
+                    turn img to np.array
+                    add to video
+        loop per word ID e.g. 01
+            array representing video frames
+            label for array
+            loop per instance of word (img) e.g. 01
+                crop img to lips
+                turn img to np.
+                
+    """
+    phrase_label_dict = {
+        "01": "stop navigation",
+        "02": "excuse me",
+        "03": "I am sorry",
+        "04": "thank you",
+        "05": "good bye",
+        "06": "i love this game",
+        "07": "nice to meet you",
+        "08": "you are welcome",
+        "09": "how are you?",
+        "10": "have a good time",
+        }
+    word_label_dict = {
+        "01": "begin",
+        "02": "choose",
+        "03": "connection",
+        "04": "navigation",
+        "05": "next",
+        "06": "previous",
+        "07": "start",
+        "08": "stop",
+        "09": "hello",
+        "10": "web",
+    }
+    videos = []
+    labels = []
+
+    dataset_dir = "data/kaggle/dataset/dataset"
+    
+    person_IDs = os.listdir(dataset_dir)
+    path = dataset_dir + "/"
+    for person_id in person_IDs[:6]:
+        print(f"person_ID:{person_id}")
+        if (os.path.isdir(dataset_dir + "/" + person_id)):
+            phrase_IDs = os.listdir(dataset_dir + "/" + person_id+"/phrases")
+            for phrase_id in phrase_IDs:
+                if (os.path.isdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id)):
+                    instance_IDs = os.listdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id)
+                    for instance_id in instance_IDs:
+                        frames = []
+                        
+                        # label = phrase_label_dict[str(instance_id)]
+
+                        if (os.path.isdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id)):
+                            label = phrase_label_dict[str(instance_id)]
+
+                            for img_path in os.listdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id):
+                                dir_path = dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id
+                                if "color" in img_path:
+                                    img = preprocess_image(dir_path + "/" + img_path)
+                                    # print(img.shape)
+                                    frames.append(img)
+
+
+                        # print(frames.shape)
+                        frame_array = np.asarray(frames)
+                        label_array = np.asarray(label)
+
+                        videos.append(frame_array)
+                        labels.append(label_array)
+    # print(len(videos[1]))
+    video_array = np.asarray(videos, dtype=object)
+    bound = int(.8 * len(video_array))
+    video_train = video_array[:bound]
+    video_test = video_array[bound:]
+    video_array = [video_train, video_test]
+
+    labels_array = np.asarray(labels, dtype=object)
+    labels_train = video_array[:bound]
+    labels_test = video_array[bound:]
+    labels_array = [labels_train, labels_test]
+    
+    np.save('output/videos.npy', video_array, allow_pickle=True)
+    np.save('output/labels.npy', labels_array, allow_pickle=True)
+            
+
+def open_data():
+    labels = np.load("output/labels.npy", allow_pickle=True)
+    bound = int(.8 * labels.shape[0])
+    Y_train = labels[:bound]
+    Y_test = labels[bound:]
+    # print(labels[:3])
+    # print(labels.shape)
+
+    videos = np.load("output/videos.npy", allow_pickle=True)
+    X_train = videos[:bound]
+    X_test = videos[bound:]
+    print(videos[:3])
+    print(videos[0].shape)
+
+    return X_train, Y_train, X_test, Y_test
+
+def load_data(batch_size, buffer_size=1024):
+    X_train, Y_train, X_test, Y_test = open_data()
+    X_train /= 255.0
+    X_test /= 255.0
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
+    train_dataset = train_dataset.shuffle(buffer_size=buffer_size).batch(batch_size, drop_remainder=True)
+
+    test_dataset = tf.data.Dataset.from_tensor_slices((X_test, Y_test))
+    test_dataset = test_dataset.shuffle(buffer_size=buffer_size).batch(batch_size, drop_remainder=True)
+
+    return train_dataset, test_dataset
+
+
+
+# open_data()
+preprocess()
+
