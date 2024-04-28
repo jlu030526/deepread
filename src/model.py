036a3b7194ab782e2cdf97ce4235e9cddee58a2b
Carson Harrell <carsonharrell@carsons-mbp-3.devices.brown.edu>
2024-04-28 15:31:53 -0400

    add train and test

--- src/model.py
+++ src/model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
 import os
-import tensorflow as tf
 import numpy as np
 
 class Model(tf.keras.Model):
@@ -29,62 +28,63 @@ class Model(tf.keras.Model):
 
         self.ff_layer = tf.keras.layers.Sequential([
             tf.keras.layers.Dense(units=0, activation='relu'),
-            tf.keras.layers.Dense(units=0, activation='relu'),
+            tf.keras.layers.Dense(units=0, activation='softmax'),
         ])
 
 
-    def call(self, inputs, is_testing=False):
-        """
-        Runs a forward pass on an input batch of images.
-        
-        :param inputs: images, shape of (num_inputs, 32, 32, 3); during training, the shape is (batch_size, 32, 32, 3)
-        :param is_testing: a boolean that should be set to True only when you're doing Part 2 of the assignment and this function is being called during testing
-        :return: logits - a matrix of shape (num_inputs, num_classes); during training, it would be (batch_size, 2)
-        """
-        # Remember that
-        # shape of input = (num_inputs (or batch_size), in_height, in_width, in_channels)
-        # shape of filter = (filter_height, filter_width, in_channels, out_channels)
-        # shape of strides = (batch_stride, height_stride, width_stride, channels_stride)
-
-        l1_out = self.conv_step(inputs, self.kernel1, False, self.conv_args_layer1, **self.conv_fns_layer1)
-        l2_out = self.conv_step(l1_out, self.kernel2, False, self.conv_args_layer2, **self.conv_fns_layer2)
-        l3_out = self.conv_step(l2_out, self.kernel3, False, self.conv_args_layer3, **self.conv_fns_layer3)
-        l4_out = self.conv_step(l3_out, self.kernel4, is_testing, self.conv_args_layer4, **self.conv_fns_layer4)
-        # stu_output = output
-        # true_output = tf.nn.conv2d(x, kernel, **conv_args)
-        # np.testing.assert_allclose(stu_output, true_output, rtol=1e-5, atol=1e-8)
-        output = tf.reshape(l4_out, (l4_out.shape[0], -1)) # make it 2D for linear layers
-
-        l1_out = self.linear_step(output, self.weights1, self.bias1)
-        l2_out = self.linear_step(l1_out, self.weights2, self.bias2)
-        l3_out = self.linear_step(l2_out, self.weights3, self.bias3, use_dropout=False, use_relu=False)
-        prbs = tf.nn.softmax(l3_out)
-        
-        return prbs
+    def call(self, inputs):
+        outputs = inputs
+        outputs = self.cnn_layer(outputs)
+        outputs = self.flatten(outputs)
+        outputs = self.rnn_layer(outputs)
+        outputs = self.ff_layer(outputs)
+        return outputs
 
     def loss(self, logits, labels):
-        """
-        Calculates the model cross-entropy loss after one forward pass.
-        Softmax is applied in this function.
-        
-        :param logits: during training, a matrix of shape (batch_size, self.num_classes) 
-        containing the result of multiple convolution and feed forward layers
-        :param labels: during training, matrix of shape (batch_size, self.num_classes) containing the train labels
-        :return: the loss of the model as a Tensor
-        """
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels, logits))
-        self.loss_list.append(loss)
-        return loss
+        # TODO: figure out a loss function
+        pass
 
 def train(model, train_inputs, train_labels):
-    #train model
-    pass
+    indices = tf.range(start=0, limit=tf.shape(train_inputs)[0], dtype=tf.int32)
+    shuffled_indices = tf.random.shuffle(indices)
+
+    train_inputs = tf.gather(train_inputs, shuffled_indices)
+    train_labels = tf.gather(train_labels, shuffled_indices)
+    accuracy_sum = 0
+    iterations = 0
+    for b, b1 in enumerate(range(model.batch_size, train_inputs.shape[0] + 1, model.batch_size)):
+        iterations += 1
+        b0 = b1 - model.batch_size
+        with tf.GradientTape() as tape:
+            y_pred = model(augment_data(train_inputs[b0:b1]), is_testing=True)
+            loss = model.loss(y_pred, train_labels[b0:b1])
+            acc = model.accuracy(y_pred, train_labels[b0:b1])
+            accuracy_sum += acc
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    average_accuracy = accuracy_sum / iterations * 1.0
+    return average_accuracy
+
+def augment_data(inputs):
+    inputs = tf.image.random_flip_left_right(inputs)
+    return inputs
 
 def test(model, test_inputs, test_labels):
-    #test model
-    pass
+    accuracy_sum = 0
+    iterations = 0
+    for b, b1 in enumerate(range(model.batch_size, test_inputs.shape[0] + 1, model.batch_size)):
+        iterations += 1
+        b0 = b1 - model.batch_size
+        y_pred = model(test_inputs[b0:b1])
+
+        acc = model.accuracy(y_pred, test_labels[b0:b1])
+        accuracy_sum += acc
+
+    average_accuracy = accuracy_sum / iterations * 1.0
+    return average_accuracy
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
 
