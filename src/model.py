107e64f61535bd999acfb0de3dd34f2e39cb7982
Alex L <alexander_liang@brown.edu>
2024-04-29 11:04:59 -0400

    model v1
    Co-authored-by: Carson Harrell <carsonmh@users.noreply.github.com>

--- src/model.py
+++ src/model.py
@@ -2,63 +2,111 @@ import tensorflow as tf
 
 import os
 import numpy as np
+from preprocess import load_data
 
 class Model(tf.keras.Model):
     def __init__(self):
         
-
+        super().__init__()
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
         self.cnn_layer = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(),
-            tf.keras.layers.MaxPool2D(),
-            tf.keras.layers.Conv2D(),
-            tf.keras.layers.MaxPool2D(),
-            tf.keras.layers.Conv2D(),
-            tf.kears.layers.MaxPool2D()
+            tf.keras.layers.Conv3D(32, (3, 3, 3), strides = 1, input_shape=(10, 100, 100, 1), activation='relu', padding='valid'), 
+            tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2), strides=2),
+            tf.keras.layers.Conv3D(64, (3, 3, 3), strides = 1, activation='relu', padding='valid'),
+            tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2), strides=2),
+            # tf.keras.layers.Conv3D(64, (2, 2, 2), activation='relu', strides=2),
+            # tf.keras.layers.MaxPool3D(pool_size=(2, 2, 2), strides=2)
         ])
+
+        self.embedding = tf.keras.layers.Embedding(10, 5) # 5 is hyperparam
 
         self.flatten = tf.keras.layers.Flatten()
 
         self.rnn_layer = tf.keras.Sequential([
-            tf.keras.layers.LSTM(),
+            tf.keras.layers.LSTM(32, return_sequences=True),
             tf.keras.layers.Dropout(.2),
-            tf.keras.layers.LSTM(),
-            tf.keras.layers.Dropout(.2)
         ])
 
-        self.ff_layer = tf.keras.layers.Sequential([
-            tf.keras.layers.Dense(units=0, activation='relu'),
-            tf.keras.layers.Dense(units=0, activation='softmax'),
+        self.ff_layer = tf.keras.Sequential([
+            tf.keras.layers.Dense(units=128, activation='relu'),
+            tf.keras.layers.Dropout(.2),
+            tf.keras.layers.Dense(units=64, activation='relu'),
+            tf.keras.layers.Dropout(.2),
+            tf.keras.layers.Dense(units=10),
         ])
 
 
     def call(self, inputs):
         outputs = inputs
         outputs = self.cnn_layer(outputs)
-        outputs = self.flatten(outputs)
+        outputs = tf.reshape(outputs, (outputs.shape[0],outputs.shape[-1], outputs.shape[2]*outputs.shape[3]))
         outputs = self.rnn_layer(outputs)
+        outputs = self.flatten(outputs)
         outputs = self.ff_layer(outputs)
+
+        # image_embedding = self.cnn_layer(inputs)
+        # word_embedding = self.embedding(caption)
         return outputs
 
-    def loss(self, logits, labels):
-        # TODO: figure out a loss function
-        pass
+    
+    def accuracy_function(self, prbs, labels):
+        # mask = tf.fill(labels.shape, True)
+        # correct_classes = tf.argmax(prbs, axis=-1) == labels
+        # boolean_vals = tf.boolean_mask(tf.cast(correct_classes, tf.float32), mask)
+        # print((list(boolean_vals)))
+        # accuracy = tf.reduce_mean(tf.boolean_mask(tf.cast(correct_classes, tf.float32), mask), keepdims=True)
 
-def train(model, train_inputs, train_labels):
-    indices = tf.range(start=0, limit=tf.shape(train_inputs)[0], dtype=tf.int32)
-    shuffled_indices = tf.random.shuffle(indices)
+        mask = tf.cast(labels >= 0, tf.bool)  # Assuming labels >= 0 for valid labels
+        correct_classes = tf.equal(tf.argmax(prbs, axis=-1), labels)
+        # print(correct_classes)
+        correct = tf.where(correct_classes).shape[0]
+        # accuracy = tf.reduce_mean(tf.cast(correct_classes, tf.float32), axis=0)
+        # print(labels.shape)
+        # print(correct)
+        accuracy = correct/labels.shape[0]
+        return accuracy
 
-    train_inputs = tf.gather(train_inputs, shuffled_indices)
-    train_labels = tf.gather(train_labels, shuffled_indices)
+# def train(model, train_inputs, train_labels):
+def train(model, trainloader):
+    # indices = tf.range(start=0, limit=tf.shape(train_inputs)[0], dtype=tf.int32)
+    # shuffled_indices = tf.random.shuffle(indices)
+
+    # train_inputs = tf.gather(train_inputs, shuffled_indices)
+    # train_labels = tf.gather(train_labels, shuffled_indices)
     accuracy_sum = 0
     iterations = 0
-    for b, b1 in enumerate(range(model.batch_size, train_inputs.shape[0] + 1, model.batch_size)):
-        iterations += 1
-        b0 = b1 - model.batch_size
+    # for b, b1 in enumerate(range(model.batch_size, train_inputs.shape[0] + 1, model.batch_size)):
+    for i, batch in enumerate(trainloader):
+        # print(f"iteration:{i}")
+        # print("here")
+        X = batch[0]
+        Y = batch[1]
+        # b0 = b1 - model.batch_size
         with tf.GradientTape() as tape:
-            y_pred = model(augment_data(train_inputs[b0:b1]), is_testing=True)
-            loss = model.loss(y_pred, train_labels[b0:b1])
-            acc = model.accuracy(y_pred, train_labels[b0:b1])
+            y_pred = model((X))
+            # print(y_pred, Y)
+            Y = tf.cast(Y, tf.int64)
+            loss = model.loss(y_pred, Y)
+            # print('loss: ', loss)
+            acc = model.accuracy_function(y_pred, Y)
+            accuracy_sum += acc
+        gradients = tape.gradient(loss, model.trainable_variables)
+        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+
+    average_accuracy = accuracy_sum / i * 1.0
+    return average_accuracy
+
+def embedding_train(model, trainloader):
+    for batch in trainloader:
+        X = batch[0]
+        Y = batch[1]
+        iterations += 1
+        # b0 = b1 - model.batch_size
+        with tf.GradientTape() as tape:
+            y_pred = model(X)
+            loss = model.loss(y_pred, Y)
+            acc = model.accuracy(y_pred, Y)
             accuracy_sum += acc
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -66,25 +114,52 @@ def train(model, train_inputs, train_labels):
     average_accuracy = accuracy_sum / iterations * 1.0
     return average_accuracy
 
-def augment_data(inputs):
-    inputs = tf.image.random_flip_left_right(inputs)
-    return inputs
 
-def test(model, test_inputs, test_labels):
+
+def test(model, testloader):
     accuracy_sum = 0
     iterations = 0
-    for b, b1 in enumerate(range(model.batch_size, test_inputs.shape[0] + 1, model.batch_size)):
+    # for b, b1 in enumerate(range(model.batch_size, test_inputs.shape[0] + 1, model.batch_size)):
+    for batch in testloader:
         iterations += 1
-        b0 = b1 - model.batch_size
-        y_pred = model(test_inputs[b0:b1])
+        # b0 = b1 - model.batch_size
+        X = batch[0]
+        Y = batch[1]
+        Y = tf.cast(Y, tf.int64)
+        y_pred = model(X)
 
-        acc = model.accuracy(y_pred, test_labels[b0:b1])
+        acc = model.accuracy_function(y_pred, Y)
         accuracy_sum += acc
 
     average_accuracy = accuracy_sum / iterations * 1.0
     return average_accuracy
 
+def main():
+    train_dataset, test_dataset = load_data(batch_size=25)
+    # print(train_dataset, test_dataset)
+    # train_inputs = train_dataset[0]
+    # train_labels = train_dataset[1]
+    # test_inputs = test_dataset[0]
+    # test_labels = test_dataset[1]
 
-# if __name__ == '__main__':
-#     main()
+    model = Model()
+    model.compile(loss=loss_fn)
+
+    epochs=10
+    for e in range(epochs):
+        train_acc = train(model, train_dataset)
+        print(f"epoch:{e}, train_acc:{train_acc}")
+
+    # print('accuracy: ', train_acc)
+    acc = test(model, test_dataset)
+    print(f"test_acc:{acc}")
+
+def loss_fn(prbs, labels):
+    # scce = tf.keras.losses.sparse_categorical_crossentropy(labels, prbs, from_logits=True)
+    labels = tf.keras.utils.to_categorical(labels, num_classes=10)
+    scce = tf.keras.losses.CategoricalCrossentropy()(labels, prbs)
+    return scce
+
+if __name__ == '__main__':
+    main()
 
--- src/preprocess.py
+++ src/preprocess.py
@@ -9,17 +9,21 @@ from PIL import Image
 #We could reference:
 #https://github.com/deepconvolution/LipNet/blob/master/codes/8_Preprocessing_model.ipynb
 
-def preprocess_image(image_path):
+def preprocess_image(image_path, width=100, height=100):
     """
     
     """
     
-    with Image.open(image_path) as img:
-        img_array = np.array(img.resize((300, 300)))
+    # with Image.open(image_path) as img:
+    #     img_array = np.array(img.resize((100, 100)))
+
 
     src = cv2.imread(image_path) 
+
     gray_image = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    img_array = np.array(gray_image.resize((300, 300)))
+    gray_image.resize((width, height))
+    # print(gray_image.shape)
+
     return gray_image
 
 
@@ -67,42 +71,57 @@ def preprocess():
         "09": "hello",
         "10": "web",
     }
+
+    
     videos = []
     labels = []
 
     dataset_dir = "data/kaggle/dataset/dataset"
     
+    category = "words"
     person_IDs = os.listdir(dataset_dir)
     path = dataset_dir + "/"
-    for person_id in person_IDs[:6]:
+    for person_id in person_IDs:
         print(f"person_ID:{person_id}")
         if (os.path.isdir(dataset_dir + "/" + person_id)):
-            phrase_IDs = os.listdir(dataset_dir + "/" + person_id+"/phrases")
+            phrase_IDs = os.listdir(dataset_dir + "/" + person_id+"/" + category)
             for phrase_id in phrase_IDs:
-                if (os.path.isdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id)):
-                    instance_IDs = os.listdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id)
+                if (os.path.isdir(dataset_dir + "/" + person_id + "/"+category+"/" + phrase_id)):
+                    instance_IDs = os.listdir(dataset_dir + "/" + person_id + "/"+category+"/" + phrase_id)
                     for instance_id in instance_IDs:
                         frames = []
                         
                         # label = phrase_label_dict[str(instance_id)]
 
-                        if (os.path.isdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id)):
-                            label = phrase_label_dict[str(instance_id)]
+                        if (os.path.isdir(dataset_dir + "/" + person_id + "/"+category+"/" + phrase_id + "/" + instance_id)):
+                            label = int(str(instance_id)) - 1
 
-                            for img_path in os.listdir(dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id):
-                                dir_path = dataset_dir + "/" + person_id + "/phrases/" + phrase_id + "/" + instance_id
+                            for img_path in os.listdir(dataset_dir + "/" + person_id + "/"+category+"/" + phrase_id + "/" + instance_id):
+                                dir_path = dataset_dir + "/" + person_id + "/"+category+"/" + phrase_id + "/" + instance_id
                                 if "color" in img_path:
                                     img = preprocess_image(dir_path + "/" + img_path)
+
                                     # print(img.shape)
                                     frames.append(img)
 
 
                         # print(frames.shape)
-                        frame_array = np.asarray(frames)
-                        label_array = np.asarray(label)
+                        if len(frames) == 10:
+                            frame_array = np.asarray(frames)
+                            # print(frame_array.shape)
+                            label_array = np.asarray(label)
+                            # print(label)
+                            videos.append(frame_array)
+                            labels.append(label_array)
+                        
 
-                        videos.append(frame_array)
-                        labels.append(label_array)
+                        # frame_array = np.asarray(frames)
+                        # print(frame_array.shape)
+                        # label_array = np.asarray(label)
+                        # print(label_array.shape)
+
+                        # videos.append(frame_array)
+                        # labels.append(label_array)
     # print(len(videos[1]))
     video_array = np.asarray(videos, dtype=object)
     bound = int(.8 * len(video_array))
@@ -111,34 +130,37 @@ def preprocess():
     video_array = [video_train, video_test]
 
     labels_array = np.asarray(labels, dtype=object)
-    labels_train = video_array[:bound]
-    labels_test = video_array[bound:]
+    labels_train = labels_array[:bound]
+    labels_test = labels_array[bound:]
     labels_array = [labels_train, labels_test]
-    
-    np.save('output/videos.npy', video_array, allow_pickle=True)
-    np.save('output/labels.npy', labels_array, allow_pickle=True)
+    # print(video_array.shape)
+    # print(labels_array.shape)
+    np.save('output/videos_train.npy', video_train, allow_pickle=True)
+    np.save('output/videos_test.npy', video_test, allow_pickle=True)
+    np.save('output/labels_train.npy', labels_train, allow_pickle=True)
+    np.save('output/labels_test.npy', labels_test, allow_pickle=True)
             
 
 def open_data():
-    labels = np.load("output/labels.npy", allow_pickle=True)
-    bound = int(.8 * labels.shape[0])
-    Y_train = labels[:bound]
-    Y_test = labels[bound:]
-    # print(labels[:3])
-    # print(labels.shape)
-
-    videos = np.load("output/videos.npy", allow_pickle=True)
-    X_train = videos[:bound]
-    X_test = videos[bound:]
-    print(videos[:3])
-    print(videos[0].shape)
+    X_train = np.load("output/videos_train.npy", allow_pickle=True)
+    X_test = np.load("output/videos_test.npy", allow_pickle=True)
+    Y_train = np.load("output/labels_train.npy", allow_pickle=True)
+    Y_test = np.load("output/labels_test.npy", allow_pickle=True)
 
     return X_train, Y_train, X_test, Y_test
 
 def load_data(batch_size, buffer_size=1024):
     X_train, Y_train, X_test, Y_test = open_data()
+
     X_train /= 255.0
     X_test /= 255.0
+    X_train = tf.convert_to_tensor(X_train, dtype=tf.float32)
+    
+    X_test = tf.convert_to_tensor(X_test, dtype=tf.float32)
+    Y_train = tf.convert_to_tensor(Y_train, dtype=tf.float32)
+    Y_test = tf.convert_to_tensor(Y_test, dtype=tf.float32)
+    X_train = np.expand_dims(X_train, axis=4)
+    X_test = np.expand_dims(X_test, axis=4)
 
     train_dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
     train_dataset = train_dataset.shuffle(buffer_size=buffer_size).batch(batch_size, drop_remainder=True)
@@ -148,8 +170,12 @@ def load_data(batch_size, buffer_size=1024):
 
     return train_dataset, test_dataset
 
+def main():
+    preprocess()
 
+if __name__ == '__main__':
+    main()
 
 # open_data()
-preprocess()
+# preprocess()
 
