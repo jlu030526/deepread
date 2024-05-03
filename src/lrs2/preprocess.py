e442e1bd96996d832e595816c70b8d7c52fba11b
Alex L <alexander_liang@brown.edu>
2024-05-03 13:45:27 -0400

    with subclips extracted

--- src/lrs2/preprocess.py
+++ src/lrs2/preprocess.py
@@ -5,24 +5,53 @@ import pandas as pd
 import imageio
 # from pylab import *
 import pylab
+import wave
+import moviepy
+from moviepy.editor import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
-def preprocess(video, timestamp_data):
-    print(timestamp_data)
+def preprocess(output_dir, video, timestamp_data):
+    # print(timestamp_data)
     words = timestamp_data["WORD"]
-    start = words = timestamp_data["START"]
+    start = timestamp_data["START"]
     end = timestamp_data["END"]
 
     word_frames_dict = {}
-    for num, image in enumerate(video.iter_data()):
-        timestamp = float(num)/ video.get_meta_data()['fps']
-        print(timestamp)
+    frames = []
+    for word, start, end in zip(words, start, end):
+        
+        frames = []
+        frame_num = int(start * video.get_meta_data()['fps'])
+        while frame_num < int(end * video.get_meta_data()['fps']):
+            frames += video.get_data(frame_num)
+            frame_num += video.get_meta_data()['fps']
+        
+        word_frames_dict[word] = frames
+
+        # out_file = output_dir+word+".mp4"
+        # ffmpeg_extract_subclip(video, start, end, targetname=out_file)
+
+    return word_frames_dict
+
+def read_audios(video_file):
+    audio_file = os.path.splitext(video_file)[0] + '.wav'
+    video_clip = VideoFileClip(video_file)
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(audio_file)
+    audio_clip.close()
+    video_clip.close()
+
+    # audio = wave.open(audio_file, 'rb')
 
 def read_videos(src):
     videos = []
     for file in os.listdir(src):
         if '.mp4' in file:
-            vid = imageio.get_reader(src+file, 'ffmpeg')
-            videos.append(vid)
+            videos.append(src+file)
+            # vid = imageio.get_reader(src+file, 'ffmpeg')
+            # videos.append(vid)
+            # read_audios(src+file)
+            # wave.open('ffaudio.wav', 'rb')
     
     return videos
 
@@ -35,19 +64,34 @@ def read_files(src):
                 src + file, sep=" ", 
                 usecols=["WORD", "START", "END", "ASDSCORE"],
                 skiprows=3)
-            print(data)
+            # print(data)
             files.append(data)
 
     return files
 
+def read_file(file_path):
+    data = pd.read_csv(
+            file_path, sep=" ", 
+            usecols=["WORD", "START", "END", "ASDSCORE"],
+            skiprows=3)
+    
+    return data
+
 def main():
     data_dir = './data/lrs2/sample/'
-    data_files = read_files(data_dir)
-    vids = read_videos(data_dir)
-    video_data_pairs = zip(os.listdir(data_dir), vids, data_files,)
-    for filename, vid, data in video_data_pairs:
-        print(filename)
-        preprocess(vid, data)
+    vid_file_path = data_dir + '00001.mp4'
+    data_file_path = data_dir + '00001.txt'
+
+    data = read_file(data_file_path)
+    preprocess(data_dir, vid_file_path, data)
+
+    # data_files = read_files(data_dir)
+    # vids = read_videos(data_dir)
+    # video_data_pairs = zip(os.listdir(data_dir), vids, data_files)
+    # for filename, vid, data in video_data_pairs:
+    #     print(filename)
+    #     word_frame_dict = preprocess(data_dir, vid, data)
+        
         # image = vid.get_data(10)
         # fig = pylab.figure()
         # fig.suptitle('image #{}'.format(10), fontsize=20)
